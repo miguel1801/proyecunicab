4af651a720e004e32592f83dcd4b32abbe2cc16d
miguel1801 <146466272+miguel1801@users.noreply.github.com>
2023-11-04 16:47:46 -0500

    Add files via upload

---
+++ pensaminto tecnologico proyecto unicab/js/index.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+
+   sr.reveal('.texto1', {
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px'
+   });
+
+
+   sr.reveal('.portamaterias', {
+
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px'
+
+   });
+
+   sr.reveal('.contenido-de-texto', {
+
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px'
+
+   });
+
+   sr.reveal('.titulo1', {
+
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px'
+
+   });
+
+   sr.reveal('.info', {
+
+    duration: 3000,
+    origin:'bottom',
+    distance: '-100px'
+
+   });
+
+   sr.reveal('.caja', {
+
+    duration: 1000,
+    origin:'bottom',
+    distance: '-100px'
+
+   });
+
+   sr.reveal('.separador', {
+
+      duration: 3000,
+      origin:'bottom',
+      distance: '-100px'
+  
+     });
+  
